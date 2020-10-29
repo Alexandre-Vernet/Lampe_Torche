9152c0db658eec2efbcea8aeaff5ba82c689f2dc
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-29 14:52:01 +0100

    Active la lampe torche à l'ouverture de l'app

--- app/src/main/java/com/ynov/vernet/lampetorche/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/lampetorche/MainActivity.java
@@ -2,6 +2,10 @@ package com.ynov.vernet.lampetorche;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            String cameraId = null;
+            try {
+                cameraId = camManager.getCameraIdList()[0];
+                camManager.setTorchMode(cameraId, true);   //Turn ON
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
