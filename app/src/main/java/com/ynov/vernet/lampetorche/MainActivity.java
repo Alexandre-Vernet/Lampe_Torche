b91278a4339df72351251bdeeaab6572be1e6e86
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-29 14:54:23 +0100

    Active la lampe torche au clic du bouton

--- app/src/main/java/com/ynov/vernet/lampetorche/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/lampetorche/MainActivity.java
@@ -7,23 +7,30 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLampeTorche;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-            String cameraId = null;
-            try {
-                cameraId = camManager.getCameraIdList()[0];
-                camManager.setTorchMode(cameraId, true);   //Turn ON
-            } catch (CameraAccessException e) {
-                e.printStackTrace();
+        btnLampeTorche = findViewById(R.id.btnLampeTorche);
+
+        btnLampeTorche.setOnClickListener(v -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                String cameraId = null;
+                try {
+                    cameraId = camManager.getCameraIdList()[0];
+                    camManager.setTorchMode(cameraId, true);   //Turn ON
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
             }
-        }
+        });
     }
 }
