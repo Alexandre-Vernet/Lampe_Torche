9abf5b3766e392cd99284f4e8d3548f9f6e56800
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-29 15:16:28 +0100

    Changement du bouton par une image Ajout d'un fond

--- app/src/main/java/com/ynov/vernet/lampetorche/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/lampetorche/MainActivity.java
@@ -5,13 +5,13 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnLampeTorche;
+    ImageView imageViewLampeTorche;
     private int compteurLampeTorche = 1;
 
     @Override
@@ -19,23 +19,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageViewLampeTorche = findViewById(R.id.btnLampeTorche);
 
-        btnLampeTorche = findViewById(R.id.btnLampeTorche);
-
-        btnLampeTorche.setOnClickListener(v -> {
+        imageViewLampeTorche.setOnClickListener(v -> {
             compteurLampeTorche++;
-            if (compteurLampeTorche % 2 == 0)
+            if (compteurLampeTorche % 2 == 0) {
+                // Changer l'image
+                imageViewLampeTorche.setImageResource(R.drawable.lampe_allumee);
+
+                // Allumer la lampe torche
                 etatLampeTorche(true);
-            else
+            } else {
+                // Changer l'image
+                imageViewLampeTorche.setImageResource(R.drawable.lampe_eteinte);
+
+                // Allumer la lampe torche
                 etatLampeTorche(false);
+            }
         });
     }
 
+    // Changer l'état de la lampe
     public void etatLampeTorche(Boolean etat) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-            String cameraId = null;
+            String cameraId;
             try {
+                assert camManager != null;
                 cameraId = camManager.getCameraIdList()[0];
                 camManager.setTorchMode(cameraId, etat);
             } catch (CameraAccessException e) {
