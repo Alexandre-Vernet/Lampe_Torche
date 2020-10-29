b4343ad9d761a00ad8c7bfd09644b62b0129ad85
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-29 15:00:11 +0100

    Active et désactive la lampe 1 fois sur 2 au clic du bouton

--- app/src/main/java/com/ynov/vernet/lampetorche/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/lampetorche/MainActivity.java
@@ -1,7 +1,5 @@
 package com.ynov.vernet.lampetorche;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
@@ -9,28 +7,40 @@ import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnLampeTorche;
+    private int compteurLampeTorche = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         btnLampeTorche = findViewById(R.id.btnLampeTorche);
 
         btnLampeTorche.setOnClickListener(v -> {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-                String cameraId = null;
-                try {
-                    cameraId = camManager.getCameraIdList()[0];
-                    camManager.setTorchMode(cameraId, true);   //Turn ON
-                } catch (CameraAccessException e) {
-                    e.printStackTrace();
-                }
-            }
+            compteurLampeTorche++;
+            if (compteurLampeTorche % 2 == 0)
+                etatLampeTorche(true);
+            else
+                etatLampeTorche(false);
         });
+    }
+
+    public void etatLampeTorche(Boolean etat) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            String cameraId = null;
+            try {
+                cameraId = camManager.getCameraIdList()[0];
+                camManager.setTorchMode(cameraId, etat);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
